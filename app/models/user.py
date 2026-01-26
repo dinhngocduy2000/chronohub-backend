f374eb783f8f8762505fd7e1d036c6e983b2f5a1
dinhngocduy2000 <ngocduydinh2000@gmail.com>
2026-01-26 23:44:49 +0700

    fix: add index to user status column and convert to enum

--- app/models/user.py
+++ app/models/user.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from typing import List
 from uuid import UUID, uuid4
 
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import Enum, String, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID as PostgreSQL_UUID
+from app.common.enum.user_status import UserStatus
 from app.common.schemas.user import UserInfo
 from app.core.database import Base
 from sqlalchemy.orm import (
@@ -19,11 +20,13 @@ class User(Base):
     id: Mapped[UUID] = mapped_column(
         PostgreSQL_UUID(as_uuid=True), primary_key=True, default=uuid4, nullable=False
     )
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    status: Mapped[str] = mapped_column(String, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=True)
-    image_url: Mapped[str] = mapped_column(String, nullable=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    status: Mapped[UserStatus] = mapped_column(
+        Enum(UserStatus), nullable=False, default=UserStatus.PENDING, index=True
+    )
+    password: Mapped[str] = mapped_column(String(16), nullable=True)
+    image_url: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
