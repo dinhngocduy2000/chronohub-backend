b98561f81c886bfa28b0f78ccd36106cecc8723f
dinhngocduy2000 <ngocduydinh2000@gmail.com>
2026-02-18 23:37:12 +0700

    fix: checking endtime after start time

--- app/services/events.py
+++ app/services/events.py
@@ -27,6 +27,10 @@ class EventService:
     ) -> EventDetailInfo:
         async def _create_event(session: AsyncSession) -> EventDetailInfo:
             try:
+                logger.info(
+                    msg=f"Checking if there is an event within the time span and same owner and group...",
+                    context=ctx,
+                )
                 event_within_time_span_same_owner_group = (
                     await self.repo.event_repo().get(
                         event_query=EventQuery(
@@ -40,23 +44,48 @@ class EventService:
                     )
                 )
                 if event_within_time_span_same_owner_group is not None:
+                    logger.error(
+                        msg=f"There is an event(s) within the time span",
+                        context=ctx,
+                    )
                     raise BadRequestException(
                         message="There is an event(s) within the time span"
                     )
-
+                logger.info(
+                    msg=f"No event(s) within the time span, checking for event duration...",
+                    context=ctx,
+                )
                 event_duration = event_create.end_time - event_create.start_time
+
+                if event_duration < timedelta(seconds=0):
+                    logger.error(msg="Event end time must be after start time")
+                    raise BadRequestException(
+                        message="Event end time must be after start time"
+                    )
+
                 if event_duration < timedelta(minutes=15):
+                    logger.error(
+                        msg=f"Event duration must be at least 15 minutes",
+                        context=ctx,
+                    )
                     raise BadRequestException(
                         message="Event duration must be at least 15 minutes"
                     )
-
+                logger.info(
+                    msg=f"Event duration is valid, creating event...",
+                    context=ctx,
+                )
                 new_event = await self.repo.event_repo().create(
                     session=session,
                     event_create=EventCreateDomain(
-                        **event_create.model_dump(mode='python'),
+                        **event_create.model_dump(mode="python"),
                         owner_id=ctx.actor,
                     ),
                     ctx=ctx,
+                )
+                logger.info(
+                    msg=f"Event created successfully: {new_event.__dict__}",
+                    context=ctx,
                 )
                 return new_event
             except Exception as e:
